fe8b25470c6ce82263a308091659cc35673494e4
Carlos Osuna <carlos.osuna@meteoswiss.ch>
2022-07-28 11:18:16 +0200

    add fortran API

---
+++ src/fortran/fdb.f90
@@ -0,0 +1,39 @@
+MODULE fdb
+   use, intrinsic :: iso_c_binding
+
+   interface
+      integer(kind=c_int) function fdb_key_add(key, param, value) bind(C, name='fdb_key_add')
+         use, intrinsic :: iso_c_binding, only : c_int, c_ptr, c_char
+         type(c_ptr), intent(in), value :: key
+
+         character(kind=c_char, len=1), dimension(*), INTENT(in) :: param
+         character(kind=c_char, len=1), dimension(*), INTENT(in) :: value
+      end function fdb_key_add
+   end interface
+
+   interface
+      integer(kind=c_int) function fdb_new_handle(fdb_handle) bind(C, name='fdb_new_handle')
+         use, intrinsic :: iso_c_binding, only : c_int, c_ptr, c_char
+
+         type(c_ptr), intent(inout) :: fdb_handle
+      end function fdb_new_handle
+   end interface
+   interface
+      integer(c_int) function fdb_new_key(key) bind(C,name='fdb_new_key')
+         use, intrinsic :: iso_c_binding, only : c_int, c_ptr, c_char
+
+         type(c_ptr), intent(inout) :: key
+
+      end function fdb_new_key
+   end interface
+
+   interface
+      integer(c_int) function fdb_archive(fdb_handle, key, data, length) bind(C, name='fdb_archive')
+         use, intrinsic :: iso_c_binding, only : c_int, c_ptr, c_char
+         type(c_ptr), intent(in), value :: fdb_handle
+         type(c_ptr), intent(in), value :: key
+         type(c_ptr), intent(in), value :: data
+         integer(kind=c_int), intent(in), value :: length
+      end function
+   end interface
+end module fdb
