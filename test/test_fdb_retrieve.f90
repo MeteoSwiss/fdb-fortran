ffd4f6dc08dab85201075a9f555d9359ded28ed9
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-08-09 11:41:03 +0200

    test_fdb_retreive

--- test/test_fdb_retrieve.f90
+++ test/test_fdb_retrieve.f90
@@ -12,6 +12,7 @@ program test_fdb_retrieve
    character(len=10)                         :: open_mode = 'r'
 
    character(kind=c_char, len=1), dimension(100)  :: string_buf
+   type(c_ptr)                               :: buf
    integer(c_long)                               :: count
    integer(c_long)                               :: read
 
@@ -36,19 +37,26 @@ program test_fdb_retrieve
 
    res = fdb_new_datareader(dr);
    res = fdb_retrieve(fdb_handle, req, dr)
-   write (*, *) 'res=', fdb_retrieve(fdb_handle, req, dr)
    res = fdb_datareader_open(dr, size)
-   write (*, *) 'size=', size
+   write (*, *) 'size of data =', size
 
    count=4
    res = fdb_datareader_read(dr, string_buf, count, read)
-   write (*, *) 'string_buf=', string_buf(1:count)
 
+   if (ALL(string_buf(:4) .eq. ['G','R','I','B'])) then 
+      write (*, *) 'PASS, string_buf=', string_buf(1:count)
+   ELSE 
+      WRITE (*, *) 'FAIL, string_buf=', string_buf(1:count)
+   END IF
 
+   res = fdb_datareader_tell(dr, read);
+   write (*, *) 'read=', read
 
-   ! res = fdb_datareader_tell(dr, read);
+   res = fdb_delete_datareader(dr);
 
-
+   ! count=4
+   ! res = fdb_datareader_read_2(dr, buf, count, read)
+   ! write (*, *) 'buf=', buf
 
    write(*,*) 'end of test_fdb_retrieve'
 end program
