bca09763e432a09519e19532f10bc28d259783f1
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-11-01 11:47:05 +0100

    flush at end of message loop for archiving

--- src/message/MessageArchiverF.cc
+++ src/message/MessageArchiverF.cc
@@ -240,8 +240,8 @@ namespace fdbf
                 count++;
                 progress(total_size);
 
-                flush();
             }
+            flush();
         }
         catch (...)
         {
